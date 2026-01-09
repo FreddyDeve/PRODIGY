53b1e31a628138d22e796fa80eed9d06e790cc08
Wilfred <wilfredsamuel0701@gamil.com>
2026-01-09 00:39:54 -0800

    add picture changes

--- portfolio.js
+++ portfolio.js
@@ -8,11 +8,44 @@ function toggleMenu() {
 hamburger.addEventListener('click', toggleMenu);
 
 
+        // TEXT ROTATOR
+const texts = ["Fullstack Software Engineer", "Web Designer", "Prompt Engineer", "Automations Engineer"];
+let currentIndex = 0;
+const textElement = document.getElementById ("text-rotator");
+
+// Fade out and Fade in
+setInterval(() => {
+    textElement.style.opacity = 0;
+    setTimeout(() => {
+        textElement.textContent = texts[currentIndex];
+        textElement.style.opacity = 0; 
+        currentIndex = (currentIndex + 1) % texts.length;
+    }, 800);
+}, 800);
+
+
+    // const rotatingTexts = ["Fullstack Software Engineer", "Web Designer", "Prompt Engineer", "Automations Engineer"];
+    // let textIndex = 0;
+    // const rotatingTextElement = document.getElementById ("text-rotator");
+
+    // rotatingTextElement.textContent = rotatingTexts[textIndex];
+    // textIndex = (textIndex + 1) % rotatingTexts.length;
+
+    // setInterval(() => {
+    //     rotatingTextElement.style.opacity = 0;
+    //     setTimeout(() => {
+    //         rotatingTextElement.textContent = rotatingTexts[textIndex];
+    //         rotatingTextElement.style.opacity = 1;
+    //         textIndexIndex = (rotatingIndex + 1) % texts.length;
+    //     }, 500);
+    // }, 500);
+
     // You-Selected SECTION
 function handleSelection (selectElement){
     let selectedValue = selectElement.value;
     alert("you selected: " + selectedValue)
 }
+
 
 
 // function showDetails(About) {
